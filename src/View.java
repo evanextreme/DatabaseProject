7d425bdfc38c09c89bc037be23cc507b6a083efb
Evan Hirsh <evanx11@gmail.com>
2018-03-19 15:41:54 -0400

    final View.java update before moving to FXML

--- src/View.java
+++ src/View.java
@@ -1,17 +1,17 @@
-package sample;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.geometry.Insets;
+import javafx.scene.text.Text;
 
 public class View {
 
     private BorderPane main = new BorderPane();
-    private HBox center = new HBox();
+    private VBox center = new VBox();
 
     public View (){
 
@@ -25,19 +25,21 @@ public class View {
     public void buildAll(){
         BorderPane border = new BorderPane();
 
-        border.setTop(drawMenu());
+        border.setTop(drawTopMenu());
 
         border.setLeft(new VBox());
-
         border.setCenter(this.center);
         border.setRight(new FlowPane());
         this.main = border;
     }
 
-    private HBox drawMenu(){
+    private HBox drawTopMenu(){
 
 
-        HBox top = new HBox();
+        HBox topMenu = new HBox();
+        topMenu.setPadding(new Insets(15, 12, 15, 12));
+        topMenu.setSpacing(10);
+        topMenu.setStyle("-fx-background-color: #336699;");
 
         Button registerBtn = new Button();
         registerBtn.setText("Register");
@@ -91,8 +93,8 @@ public class View {
             }
         });
 
-        top.getChildren().addAll(registerBtn, catalogueBtn, discountsBtn, returnBtn, totalBtn);
-        return top;
+        topMenu.getChildren().addAll(registerBtn, catalogueBtn, discountsBtn, returnBtn, totalBtn);
+        return topMenu;
     }
 
     private void drawRegister(){
@@ -157,23 +159,62 @@ public class View {
         title.setText("Total");
         title.setFont(Font.font ("Verdana", 20));
         title.setPadding(new Insets(10, 10, 10, 10));
-        center.getChildren().addAll(title,drawTotalList());
+
+        HBox viewBox = new HBox();
+        viewBox.getChildren().addAll(drawTotalList(), drawTotalSidebar());
+
+        center.getChildren().addAll(title,viewBox);
     }
 
     private GridPane drawTotalList(){
-        GridPane list = new GridPane();
-        list.setPadding(new Insets(10));
-        list.setGridLinesVisible(true);
+        GridPane totalList = new GridPane();
+        totalList.setPadding(new Insets(10));
+        totalList.setGridLinesVisible(true);
         ColumnConstraints c1 = new ColumnConstraints();
         c1.setPercentWidth(33);
         ColumnConstraints c2 = new ColumnConstraints();
         c2.setPercentWidth(33);
         ColumnConstraints c3 = new ColumnConstraints();
         c3.setPercentWidth(33);
-        list.getColumnConstraints().addAll(c1, c2, c3);
-        list.add(new Label("Name"), 0, 0);
-        list.add(new Label("Qty"), 1, 0);
-        list.add(new Label("Total"), 2, 0);
-        return list;
+        totalList.getColumnConstraints().addAll(c1, c2, c3);
+        totalList.add(new Label("Name"), 0, 0);
+        totalList.add(new Label("Qty"), 1, 0);
+        totalList.add(new Label("Total"), 2, 0);
+        return totalList;
+    }
+
+    private VBox drawTotalSidebar(){
+        VBox totalSidebar = new VBox();
+        Button cashBtn = new Button("Cash");
+        cashBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Cash Clicked");
+            }
+        });
+        Button creditBtn = new Button("Credit");
+        creditBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Credit Clicked");
+            }
+        });
+        totalSidebar.getChildren().addAll(cashBtn, creditBtn, drawMoneyBox());
+        return totalSidebar;
+    }
+
+    private GridPane drawMoneyBox(){
+        GridPane moneyBox = new GridPane();
+        moneyBox.setStyle("-fx-background-color: #336699;");
+        moneyBox.add(new Label("Total    ="), 0, 0);
+        moneyBox.add(new Label("Tax      ="), 0, 1);
+        moneyBox.add(new Label("Subtotal ="), 0, 2);
+        TextField totalField = new TextField("0");
+        moneyBox.add(totalField,1,0);
+        TextField taxField = new TextField("0");
+        //moneyBox.add(taxField, 1, 1);
+        TextField subtotalField = new TextField("0");
+        //moneyBox.add(subtotalField, 1, 2);
+        return moneyBox;
     }
 }
