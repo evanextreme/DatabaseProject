c4b0b33b2c0bab6030e1dbfc1df644a5856a658a
theodorabendlin <trb7281@cs.rit.edu>
2018-04-10 19:53:56 -0400

    Added in controller, DAO and service files

---
+++ src/main/java/com/bobby/tables/RetailStore/service/BrandService.java
@@ -0,0 +1,19 @@
+package com.bobby.tables.RetailStore.service;
+
+import com.bobby.tables.RetailStore.models.Brand;
+import com.bobby.tables.RetailStore.repository.BrandDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+//import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BrandService {
+	
+	@Autowired
+	private BrandDAO brandDAO;
+
+}
