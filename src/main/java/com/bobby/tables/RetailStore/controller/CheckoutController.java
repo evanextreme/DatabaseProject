e4ebf600c56ec8e9a1597e1bcd51dcb6d7b04768
Stephen Cerbone <importsjc@gmail.com>
2018-04-14 14:26:56 -0400

    Fixing POST call, still not working though.

--- src/main/java/com/bobby/tables/RetailStore/controller/CheckoutController.java
+++ src/main/java/com/bobby/tables/RetailStore/controller/CheckoutController.java
@@ -2,8 +2,12 @@ package com.bobby.tables.RetailStore.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,8 +51,10 @@ public class CheckoutController {
 	
 	//checkout?discountField=0
 	@RequestMapping(value="/checkout", method = RequestMethod.POST, params={"discountField"})
-	public String test(Model model, @RequestParam(value = "discountField") int discountID) {
+	public String test(Model model, @RequestBody String json, HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "discountField") int discountID) {
 		//discountID is 0 if no discount is applied
+		System.out.println("json: " + json);
+		
 		model.addAttribute("items", ProductDAO.getAllProducts());
 		model.addAttribute("cart", BrandDAO.getAllBrands());
 		model.addAttribute("discounts", DiscountDAO.getAllDiscounts());
